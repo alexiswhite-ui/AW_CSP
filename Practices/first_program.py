dfc35295807113d7eb986acfdd2169d16533f207
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-01-22 14:42:32 -0700

    edited practices

--- Practices/first_program.py
+++ Practices/first_program.py
@@ -1,2 +1,18 @@
  # AW First Program
 
+sport = "Volleyball"
+
+time = 2"hr"
+
+enjoyment = "intermidaite"
+
+what_day = "tuesday and thursday"
+
+statement = "I have practice for"
+
+word = "with"
+
+
+
+
+print(statement, sport, what_day, word
