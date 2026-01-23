c910322c1329b5262e88bfbe949beacc04034949
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-01-23 14:42:21 -0700

    numbers notes

---
+++ Practices/integer_floats.py
@@ -0,0 +1,16 @@
+'''price = float(input("How much did it cost: $"))
+
+sales_tax = float(input ("How much is the sales tax: %"))/100
+
+
+total = round(price + (price*sales_tex), 2)
+
+
+print(total)'''
+
+
+apples = int(input("How many apples are there:"))
+
+friends = 8
+
+print("each friends can have", apples//friends, "amd I have", apples%friends, "left over")
