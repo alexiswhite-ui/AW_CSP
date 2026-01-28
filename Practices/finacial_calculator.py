c05dd49c8f7c64835c21ee342fc9e83bafe0ba4b
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-01-28 14:13:51 -0700

    finished finacial calculator

--- Practices/finacial_calculator.py
+++ Practices/finacial_calculator.py
@@ -12,12 +12,12 @@ car = float(input("What is your monthly transportation: $"))
 
 print(income, rent, utilities, food, car)
 
-print("your rent is $", rent, "and that is", (rent/income)*100, "% of your income")
+print("your rent is $", rent, "and that is", round(rent/income, 2)*100, "% of your income")
 
-print("your utilities are $", utilities, "and that is", (utilities/income)*100, "% of your income") 
+print("your utilities are $", utilities, "and that is", round(utilities/income, 2)*100, "% of your income") 
 
-print("your groceries are $", food, "and that is", (food/income)*100, "% of your income")
+print("your groceries are $", food, "and that is", round(food/income, 2)*100, "% of your income")
 
-print("your transportations are $", car, "and that is", (car/income)*100, "% of your income")
+print("your transportations are $", car, "and that is", round(car/income, 2)*100, "% of your income")
 
-print("you have $"())
+print("you have $", round(income*.1, 2), "left to spend!")
---
+++ Practices/silly_sentences.py
@@ -0,0 +1,11 @@
+# AW p7 sillly sentences
+
+place = input("A place: ")
+
+verb1 = input("A ver: ")
+
+feeling = input("A feeling: ")
+
+verb2 = input("A verb: ")
+
+print("At"+ " " + place + " " +)
