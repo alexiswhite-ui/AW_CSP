87d50a3d7384f4f00556f329a9a0a074f89b4099
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-01-28 14:18:13 -0700

    finished finacial calucaltor

--- Practices/finacial_calculator.py
+++ Practices/finacial_calculator.py
@@ -10,6 +10,8 @@ food = float(input("What is your monthly groceries: $"))
 
 car = float(input("What is your monthly transportation: $"))
 
+savings =  round(income*.1, 2)
+
 print(income, rent, utilities, food, car)
 
 print("your rent is $", rent, "and that is", round(rent/income, 2)*100, "% of your income")
@@ -20,4 +22,6 @@ print("your groceries are $", food, "and that is", round(food/income, 2)*100, "%
 
 print("your transportations are $", car, "and that is", round(car/income, 2)*100, "% of your income")
 
-print("you have $", round(income*.1, 2), "left to spend!")
+print("you should be saving $", savings)
+
+print("you will now have $", (income-food-car-utilities-rent-savings), "left to spend!")
