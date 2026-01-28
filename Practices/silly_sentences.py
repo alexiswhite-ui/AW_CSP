80ae9302e3bdfcbf8e1927d33f7230ee1b84588c
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-01-28 14:33:15 -0700

    finished silly sentences

--- Practices/silly_sentences.py
+++ Practices/silly_sentences.py
@@ -1,17 +1,11 @@
 # AW p7 sillly sentences
 
-place = input("A place: ")
+place = input("A place: ").capitalize()
 
-verb1 = input("A verb: ")
+verb1 = input("A verb-ing: ").lower()
 
-feeling = input("A feeling: ")
+feeling = input("A feeling: ").lower()
 
-verb2 = input("A verb: ")
+verb2 = input("A verb: ").lower()
 
-print("At"+ " " + place + " " + "in the sun")
-
-print("We'll be" + " " + verb1 + " " + "and having fun")
-
-print("And it makes me feel so" + " " + feeling)
-
-print("I" + " " + verb2)
+print("At"+ " " + place + " " + "in the sun" + " " + "we'll be" + " " + verb1 + " " + "and having fun" + " " + "and it makes me feel so full of" + " " + feeling + " " + "I" + " " + verb2)
