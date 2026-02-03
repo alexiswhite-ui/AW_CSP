77292e8d082bdf2770bd8b458e3764a7bb7e50a0
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-02-03 14:41:59 -0700

    fizzbuzz

---
+++ Practices/fizzbuzz.py
@@ -0,0 +1,19 @@
+# AW 7 fizz buzz
+
+start = 1
+
+while start <= 50:
+    print(start)
+    start += 1
+    if start%3 == 0:
+        print("fizz")
+    elif start%5 == 0:
+        print("buzz")
+    elif start%15 == 0:
+        print("fizzbuzz")
+
+
+
+
+
+
