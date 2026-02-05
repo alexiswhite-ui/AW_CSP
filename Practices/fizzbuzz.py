e4f4388bdc0a39e785c3be578407f55be84eb263
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-02-05 14:14:09 -0700

    fizz buzz

--- Practices/fizzbuzz.py
+++ Practices/fizzbuzz.py
@@ -3,14 +3,15 @@
 start = 1
 
 while start <= 50:
-    print(start)
-    start += 1
     if start%3 == 0:
         print("fizz")
     elif start%5 == 0:
         print("buzz")
     elif start%15 == 0:
         print("fizzbuzz")
+    else:
+        print(start)
+    start += 1
 
 
 
