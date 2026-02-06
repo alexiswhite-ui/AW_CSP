4c1dd34342dde31c26e491bf63c4e4a9cd9dcb40
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-02-06 14:41:19 -0700

    game

---
+++ Practices/team_game.py
@@ -0,0 +1,14 @@
+# AW p7 team game
+
+import random #4 letter word
+print("  ------")
+print("  |     |")
+print("  |")
+print("  |")
+print("  |")
+print("  |")
+print("_____")
+
+def guess(letter):
+    return f"{letter}"
+print(guess().random)
