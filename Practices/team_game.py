c7b046a89f168cf19315d196799a55c6d5de4458
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-02-09 14:41:54 -0700

    game
    Co-authored-by: something7777 <something7777@users.noreply.github.com>

--- Practices/team_game.py
+++ Practices/team_game.py
@@ -1,6 +1,6 @@
 # AW p7 team game
 
-import random #4 letter word
+'''import random #4 letter word
 print("  ------")
 print("  |     |")
 print("  |")
@@ -11,4 +11,14 @@ print("_____")
 
 def guess(letter):
     return f"{letter}"
-print(guess().random)
+print(guess().random) 
+
+'''
+
+number = [1,2,3,4,5,6,7,8,9]
+print(f"{number[0]}|{number[1]}|{number[2]}")
+print(f"_ _ _")
+print(f"{number[3]}|{number[4]}|{number[5]}")
+print(f"_ _ _")
+print(f"{number[6]}|{number[7]}|{number[8]} ")
+
