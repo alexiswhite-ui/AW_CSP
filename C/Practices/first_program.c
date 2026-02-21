d60ceec3c0f5dbebfeb265d3e17ea47f87fece4c
barrels_volleyall <alexis.white@ucas-edu.net>
2026-02-21 06:56:36 +0000

    first program

---
+++ C/Practices/first_program.c
@@ -0,0 +1,12 @@
+// AW p7 first program
+#include <studio.h>
+
+int main(){
+    printf("Enter in your name: ");
+    char name[15];
+    scanf("%s", name);
+
+    printf("hello, %s welcome to your first c program.\n", name);
+    return 0;
+
+}
