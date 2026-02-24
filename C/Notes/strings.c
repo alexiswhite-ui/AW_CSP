ee2b942b6180e30a6cdd4cd708ac090b9036a409
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-02-24 14:14:29 -0700

    cal

---
+++ C/Notes/strings.c
@@ -0,0 +1,13 @@
+// AW strings notes
+#include <stdio.h>
+
+int main(){
+    char subject[] = "Computer Science Principles";
+    char school[] = "UCAS";
+    char book[50];
+
+    printf("This is %s at %s. It is awesome!\n", subject, school);
+    
+
+    return 0;
+}
