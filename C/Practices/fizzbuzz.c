544bbfacf3ad04a0adf008af87b2c5f448a2d86b
barrels_volleyall <alexis.white@ucas-edu.net>
2026-03-05 19:27:53 +0000

    fizzbuzz

---
+++ C/Practices/fizzbuzz.c
@@ -0,0 +1,18 @@
+// AW p7 fizzbuzz
+#include <stdio.h>
+int main(){
+    int i = 0;
+
+for(i ; i <50 ; i++){
+    if(i %3 == 0 && i %5 == 0){
+        printf("FizzBuzz\n");
+    } else if( i %3 == 0){
+        printf("Fizz\n");
+    } else if(i %5 ==0){
+        printf("Buzz\n");
+    } else{
+        printf("%d\n", i);
+    }
+}
+return 0;
+}
