096681f2bda87b2c522f3a74d61fd4110de17b33
barrels_volleyall <alexis.white@ucas-edu.net>
2026-03-08 05:16:29 +0000

    calculator

--- C/Notes/array.c
+++ C/Notes/array.c
@@ -1,14 +1,25 @@
 // AW p7 arrays and loops
 
 int main(){
+    // Example 1
     int grades [] = {78,18,99,87,94,86,88,90};
     printf("%d\n", grades[1]);
     grades[1] = 70;
     printf("%d\n", grades[1]);
 
+    // Example 2
     float measurements [10];
     measurements [0] = 5.25;
     measurements [1] = 0.35;
     measurements [2] = 1.99;
     measurements [3] = 3.48;
+
+    printf("%.2f %.2f %.2f %.2f %.2f \n", measurements [0], measurements [1], measurments [2], measurements [3])
+
+    //Example 3
+    char names[] [20] = {"alex", "Katie", "andrew", "vienna", "tia", "treyson"};
+
+    printf("%s\n", names [6]);
+
+    return 0;
 }
--- C/Practices/updated_financial_cal.c
+++ C/Practices/updated_financial_cal.c
@@ -1,47 +1,48 @@
-// AW p7 updated financial calculator
+//AW p7 updated finacial calculator
 #include <stdio.h>
 
- float payment (char* cost){
-    float monthly;
-    printf("what is your monthly %s", cost);
-    scanf("%f", &monthly);
-    return monthly;
-  }
-  float income;
-  float rent;
-  float utilities;
-  float groceries;
-  float transport;
+float income, rent, utilities, groceries, transport;
+float rent_percent, utilities_percent, groceries_percent, transportation;
+float savings, savings_percent, spending_money;
 
-float percent(){
-    
+void monthly(){
+  printf("monthly income: ");
+  scanf("%f", &income);
+  printf("monthly rent/morage: ");
+  scanf("%f", &rent);
+  printf("monthly utilities: ");
+  scanf("%f", &utilities);
+  printf("monthly groceries: ");
+  scanf("%f", &groceries);
+  printf("monthly transportation: ");
+  scanf("%f", &transport);
+
 }
-int morgage = (rent/income*100);
-int bill = (utilities/income*100);
-int food = (groceries/income*100);
-int car = (transport/income*100);
 
-int main(){
-    income = payment("income:");
-    
-    rent = payment("rent/morgage: ");
-    
-    utilities = payment("utilities: ");
-
-    groceries = payment("groceries ");
-
-    transport = payment("transportation: ");
+void percent(){
+  rent_percent = (rent/income) * 100;
+  utilities_percent = (utilities/income) * 100;
+  groceries_percent = (groceries/income) * 100;
+  transportation = (transport/income) * 100;
+  savings_percent = 10.0;
+  savings = (savings_percent / 100) * income;
+  spending_money = income - (rent + utilities + groceries + transportation + savings);
+}
 
 
-    
+int main(void){
 
-    printf("Your rent is %.2f which is %.1d percent of your income\n", rent, morgage);
+  monthly();
+  percent();
 
-    printf("Your utilities is %.2f which is %.1d percent of your income\n", utilities, bill);
+  printf("your rent is $%.2f and that is %.0f of your income\n", rent, rent_percent);
+  printf("your utilities is $%.2f and that is %.0f of your income\n", utilities, utilities_percent);
+  printf("your groceries is $%.2f and that is %.0f of your income\n", groceries, groceries_percent);
+  printf("your transportation is $%.2f and that is %.0f of your income\n", transport, transportation);
+  printf("you should save $%.2f a month, that is %.0f of your income\n", savings, savings_percent);
+  printf("you have $%.2f of spending each month\n", spending_money);
 
-    printf("Your groceries is %.2f which is %.1d percent of your income\n", groceries, food);
 
-    printf("Your transportation is %.2f which is %.1d percent of your income", transport, car);
-
-    return 0;
+  return 0;
+  
 }
