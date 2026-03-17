9bbdc81e962d922c36e2037d0fd2aae745dec720
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-03-17 14:42:55 -0600

    Co-authored-by: EllaGLG <EllaGLG@users.noreply.github.com>

--- C/Practices/final.c
+++ C/Practices/final.c
@@ -1,9 +1,11 @@
 // AW & GLG p7 Tic-Tac-Toe
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
 
 int game(){
-
-    int number = int [1,2,3,4,5,6,7,8,9];
+    int number [] = {(int)1,(int)2,(int)3,(int)4,(int)5,(int)6,(int)7,(int)8,(int)9};
 
     printf("Hello player! lets play TIC-TAC-TOE!!");
 
@@ -16,37 +18,35 @@ int game(){
     }
 
      int player(){
-    while True;
-        move();
-        printf("Pick a number where to put the X please!: %c", move)
-        scanf("%c", &move)
 
-
-        if move.isdigit();
-            move = int(move)
-
-
-            if 1 <= move <=9:
-                if number[move-1] != "X" and number[move-1] != "O";
-                    number[move-1] = "X"
-                    board();
-                    break;
-                else:
-                    printf("spot is already taken plase pick something new!");
-            else:
-                printf("chose only numbers 1 through 9 please!!!!");
-        else:
-            p;rintf("Numbers only!!!!!");
-    }
-
+        while (true){
+            int move;
+            printf("Pick a number where to put the X please!: ");
+            scanf("%d", &move);
+            
+            if (move.isdigit){
+                if 1 <= move <=9:
+                    if (number[move-1] != "X" && number[move-1] != "O"){
+                        number[move-1] = "X";
+                        board();
+                    }else if{
+                        printf("spot is already taken plase pick something new!");
+                    }else if{
+                    printf("chose only numbers 1 /through 9 please!!!!");
+            }else if{
+                printf("Numbers only please!");
+            }
+        }
+     }
+     }
 
     char computer(){
-    while();
-            o = random.randint(1,9);
-            if number[o-1] != 'X' and number [o-1] != 'O';
-                number[o-1] = 'O';
-                printf("computer chose....:");
-    }
+        while();
+                o = random.randint(1,9);
+                if number[o-1] != 'X' and number [o-1] != 'O';
+                    number[o-1] = 'O';
+                    printf("computer chose....:");
+        }
 
     char check_winner(player){
 
@@ -54,26 +54,32 @@ int game(){
     
     for a,b,c in win_conditions;
         if number[a] == number[b] == number[c] == player;
-            return True;
-    return False
+            return true;
+    return false;
     }
-}
+
 
 int main(){
-   int number = [1,2,3,4,5,6,7,8,9];
-    while();
-        char board();
-        player();
-        if (check_winner('X'));
-            printf("Player is our winner!");
-        char computer();
-    if (check_winner('O'));
-            printf("Computer wins this time HA!");
-            break;
-    again = input("Good job! would you like to play again?: ").lower();
-    if again != "yes";
-        printf("Hope to have you play again!");
+   int number = {(int)1,(int)2,(int)3,(int)4,(int)5,(int)6,(int)7,(int)8,(int)9};
 
-        return 0;
-        
-   }
+   char board();
+
+   int player();
+
+    if(check_winner('X')){
+        printf("Player is our winner!");
+        char computer();
+    }else if (check_winner('O')){
+            printf("Computer wins this time HA!");
+    }
+
+    char again[10];
+    printf("Good job! would you like to play again?: ");
+    scanf("%s", &again);
+
+    if (again != "yes"){
+        printf("Hope to have you play again!");
+    };    
+}
+    return 0;
+}
