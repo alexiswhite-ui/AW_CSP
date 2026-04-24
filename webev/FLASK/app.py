55a0920c55bbdd39132daaa9292bc33a5738855b
alexiswhite-ui <alexis.white@ucas-edu.net>
2026-04-24 14:42:13 -0600

    flask

---
+++ webev/FLASK/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+admin = True
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="Home", content="This is the home page!")
+
+@app.route("/test")
+def testing():
+    return render_template("index.html", title="test", content="This is to make sure it works!")
+
+@app.route("/admin")
+def administrator():
+    if admin:
+        return "You are an admin"
+    else:https://ucas-edu.instructure.com/courses/4362
+        return redirect (url_for("home"))
+    
+@app.route("/<name>")
+def person(name):
+    return f"<h1>Hello {name}!</h1>"
+
+
+app.run()
